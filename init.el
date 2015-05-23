43e271b160205ab21399f6223ecda841b0cd4f63
xinming90 <panxinming90@gmail.com>
2015-05-23 17:52:01 +0800

    comment-dwim-line

--- init.el
+++ init.el
@@ -57,6 +57,22 @@
 (display-time)
 
 
+;; comment-dwim
+;; Original idea from
+;; http://www.opensubscriber.com/message/emacs-devel@gnu.org/10971693.html
+(defun comment-dwim-line (&optional arg)
+  "Replacement for the comment-dwim command.
+  If no region is selected and current line is not blank and we are not at the end of the line,
+  then comment current line.
+  Replaces default behaviour of comment-dwim, when it inserts comment at the end of the line."
+  (interactive "*P")
+  (comment-normalize-vars)
+  (if (and (not (region-active-p)) (not (looking-at "[ \t]*$")))
+      (comment-or-uncomment-region (line-beginning-position) (line-end-position))
+    (comment-dwim arg)))
+(global-set-key (kbd "C-[ [ ;") 'comment-dwim-line)
+
+
 ;; c/c++
 (add-hook 'c-mode-hook 'auto-complete-mode)
 (add-hook 'c++-mode-hook 'auto-complete-mode)
