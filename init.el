ab90006ebf329c11170c5d64eda00802ceaa5d52
xinming90 <panxinming90@gmail.com>
2017-04-26 10:25:03 +0800

    set-key "C-c gf" magit-log-buffer-file

--- init.el
+++ init.el
@@ -336,7 +336,7 @@
 ;; magit
 (global-set-key (kbd "C-c g s") 'magit-status)
 (global-set-key (kbd "C-c g l") 'magit-log-all)
-(global-set-key (kbd "C-c g f") 'magit-current-file-log)
+(global-set-key (kbd "C-c g f") 'magit-log-buffer-file)
 (global-set-key (kbd "C-c g d u") 'magit-diff-unstaged)
 (global-set-key (kbd "C-c g d s") 'magit-diff-staged)
 
