a7291bb9c70033960c9cef047d7c8a31431c7dbc
xinming90 <panxinming90@gmail.com>
2017-04-28 09:34:41 +0800

    elpa upgrade all packages

---
+++ elpa/company-irony-20170411.1645/company-irony-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "company-irony" "20170411.1645" "company-mode completion back-end for irony-mode" '((emacs "24.1") (company "0.8.0") (irony "0.3.0") (cl-lib "0.5")) :url "https://github.com/Sarcasm/company-irony/" :keywords '("convenience"))
---
+++ elpa/pylint-20170402.555/pylint-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "pylint" "20170402.555" "minor mode for running `pylint'" 'nil :keywords '("languages" "python"))
